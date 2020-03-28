72f2d0b5b91a9c1278310a7e1cfc20d4310cdcda
lfrodriguez <lfrodriguez06@gmail.com>
2020-03-27 22:47:37 -0600

    added the heroku domain to the res.cookie in app.post(/auth/sign-in)

--- src/server/server.js
+++ src/server/server.js
@@ -71,7 +71,7 @@ app.post('/auth/sign-in', async (req, res, next) => {
         res.cookie('token', token, {
           httpOnly: !(env === 'development'),
           secure: !(env === 'development'),
-          domain: 'https://platzivideoapptest.herokuapp.com/',
+          domain: 'https://shielded-caverns-26539.herokuapp.com/',
         });
 
         res.status(200).json(user.user);
